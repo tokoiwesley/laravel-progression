c6555c3a0f8687c01e74eaf76fd94521a5b06e47
Wesley Tokoi <tokoiwesley@gmail.com>
2019-01-25 11:19:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Laravel Reference Numbers
+This is a package for generation of unique and sequential reference numbers (codes) for any given table records.  
+## Installation
+
+## Usage
+
+## License
+
---
+++ src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tokoiwesley\ReferenceNumbers;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
